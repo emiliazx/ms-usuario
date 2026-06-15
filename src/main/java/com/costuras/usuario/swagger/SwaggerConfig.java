5e9ad3a469518a622fb26ce243b27b632a8e1938
emilia <emiliazamo2017@gmail.com>
2026-06-15 12:00:56 -0400

    clase swagger

--- src/main/java/com/costuras/usuario/swagger/SwaggerConfig.java
+++ src/main/java/com/costuras/usuario/swagger/SwaggerConfig.java
@@ -1,5 +1,21 @@
 package com.costuras.usuario.swagger;
 
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("MS Usuario").description("Gestión de direcciones e historial de compras del usuario.").version("1.0.0"))
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new Components().addSecuritySchemes("BearerAuth",
+                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
 
 }
